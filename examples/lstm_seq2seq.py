cf59f751c3c19708bee595433e91745b592bf031
mightoz <oscar.beronius@gmail.com>
2018-12-17 14:07:28 +0100

    Tweak params.

--- examples/lstm_seq2seq.py
+++ examples/lstm_seq2seq.py
@@ -57,7 +57,7 @@ from os.path import dirname, join
 current_dir = dirname(__file__)
 
 batch_size = 64  # Batch size for training.
-epochs = 100  # Number of epochs to train for.
+epochs = 1  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
