9fc0b42c342ef618ef8efb4e70bb1599e042fa62
mightoz <oscar.beronius@gmail.com>
2018-12-17 10:57:43 +0100

    make vscode understand data path.

--- examples/lstm_seq2seq.py
+++ examples/lstm_seq2seq.py
@@ -53,20 +53,23 @@ from __future__ import print_function
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
 import numpy as np
+from os.path import dirname, join
+current_dir = dirname(__file__)
 
 batch_size = 64  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
-data_path = 'fra-eng/fra.txt'
+data_path = '/fra-eng/fra.txt'
+file_path = current_dir+data_path
 
 # Vectorize the data.
 input_texts = []
 target_texts = []
 input_characters = set()
 target_characters = set()
-with open(data_path, 'r', encoding='utf-8') as f:
+with open(file_path, 'r', encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines[: min(num_samples, len(lines) - 1)]:
     input_text, target_text = line.split('\t')
